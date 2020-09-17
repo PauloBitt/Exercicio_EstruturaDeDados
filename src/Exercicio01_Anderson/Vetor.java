59e35548fa197201c37c7afe5994d6107da0c4be
Paulo Bittencourt <paulo.bittencourt@souunit.com.br>
2020-09-17 20:09:16 -0300

    Commit adicionando os (AdicionarInicio e AdicionarPos)

--- src/Exercicio01_Anderson/Vetor.java
+++ src/Exercicio01_Anderson/Vetor.java
@@ -31,10 +31,91 @@ public class Vetor<T> {
         
         //Novo Vetor Génerico
         vetor = (T[]) new Object[tamanho];
-        
+       
         this.qntElementos = tamanho;
         
     }
+    
+    public void adicionaInicio(T elemento){
+        if (tamanho() >= vetor.length-1){
+        redimensionar(vetor);
+    }
+    Iterator iter = new Iterador(vetor);
+    int i = 0;
+    T empurrar1 = vetor[0];
+    T empurrar2 = empurrar1;
+    
+    while (iter.hasNext()){
+        if(i < tamanho() && vetor[i] != null){
+            if (vetor[i + 1] == null){
+                empurrar2 = vetor[i + 1];
+                vetor[i + 2] = empurrar1;
+                break;
+            }
+            empurrar2 = vetor[i + 1];
+            vetor[i + 1] = empurrar1;
+            empurrar1 = empurrar2;
+        } else{
+            break;
+        }
+        i++;
+    }
+    vetor[0] = elemento;
+    qntElementos++;
+    
+   }
+    
+    public void adicionaPosicao(T elemento, int pos){
+        if (tamanho() >= vetor.length-1){
+        redimensionar(vetor);
+    }
+    if ((pos < 0 && pos > vetor.length)){
+        throw new ArrayIndexOutOfBoundsException("Posição Invalida");
+    }
+    Iterator iter = new Iterador(vetor);
+    int i = 0;
+    int count = 0;
+    T empurrar1 = vetor[0];
+    T empurrar2 = empurrar1;
+    
+    if (!existeDado(pos)){
+        vetor[pos] = elemento;
+        qntElementos++;
+    } else if (existeDado(pos)){
+        while (iter.hasNext()){
+            if(count >= pos && count <= tamanho()){
+                if (vetor[i + 1] == null){
+                    empurrar2 = vetor[i + 1];
+                    vetor[i + 1] = empurrar1;
+                    break;
+                }
+                i++;
+                count++;
+            }
+            vetor[pos] = elemento;
+            qntElementos++;
+        }
+    }
+    
+    while (iter.hasNext()){
+        if(i < tamanho() && vetor[i] != null){
+            if (vetor[i + 1] == null){
+                empurrar2 = vetor[i + 1];
+                vetor[i + 2] = empurrar1;
+                break;
+            }
+            empurrar2 = vetor[i + 1];
+            vetor[i + 1] = empurrar1;
+            empurrar1 = empurrar2;
+        } else{
+            break;
+        }
+        i++;
+    }
+    vetor[0] = elemento;
+    qntElementos++;
+    
+   }
     
     public boolean existeDado(int posicao){
         return vetor[posicao] != null;
@@ -51,40 +132,24 @@ public class Vetor<T> {
         return vetor[posicao];
     }
     
-    public void redimensionar(){
-        if(this.qntElementos == vetor.length){
-            Vetor[] newArray = new Vetor[vetor.length * 2];
-            for(int i=0; i < vetor.length; i++){
-                newArray[i] = (Vetor)this.vetor[i];
-                System.out.println(newArray[i]);
-            }
-            for (int h = 0; h < newArray.length; h++){
-                this.vetor[h] = (T) newArray[h];
-            }
+    public void redimensionar(T[] vetor){
+        T[] newVetor = (T[]) new Object[tamanho() * 2];
+        for (int i = 0; i <= tamanho(); i++){
+            newVetor[i] = vetor[i];
         }
+        this.vetor = newVetor;
     }
     
-    public void adicionar(T elemento){
-        if(tamanho() == vetor.length){
-            System.out.println("Redimencionando" + " " + tamanho());
-            redimensionar();
-        }
-    }
     
-    public void removerFim(){
-        for(int i = 0; i < vetor.length; i++){
-            if (vetor[i] == i){
-                
-            }
-        }
-    }
+//    public void removerFim(){
+//        for(int i = 0; i < vetor.length; i++){
+//            if (vetor[i] == i){
+//                
+//            }
+//        }
+//    }
     
-    public void adicionaPos(T elemento, int posicao){
-        if (tamanho() >= vetor.length-1){
-        redimensionar(vetor);
-    }
-   }
+
     
-    Iterador it = new Iterador(vetor);
     
 }
